8eefbb080931885a52a772fa2eba5488c4d6ee30
Maciej Kozuszek <maciejkozuszek@pop-os.localdomain>
2022-12-11 21:00:15 +0100

    Part 2

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -9,15 +9,23 @@ fn unique(s: &str) -> Option<(usize, usize, char)> {
 }
 
 pub fn part1(input: String) -> usize {
+    characters_to_proceed(input, 4)
+}
+
+pub fn part2(input: String) -> usize {
+    characters_to_proceed(input, 14)
+}
+
+pub fn characters_to_proceed(input: String, message_length: usize) -> usize {
     let data = input.char_indices().collect::<Vec<(usize, char)>>();
-    data.windows(4)
+    data.windows(message_length)
         .find_map(|window| {
             let string = window.iter().fold("".to_owned(), |mut acc, next| {
                 acc.push(next.1);
                 acc
             });
             match unique(&string) {
-                None => Some(window[3].0 + 1),
+                None => Some(window[message_length - 1].0 + 1),
                 Some(_) => None,
             }
         })
