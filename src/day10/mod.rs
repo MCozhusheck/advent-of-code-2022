ac7b683918da09e2638df2f3946635b9319a9776
Maciej Kozuszek <maciejkozuszek@pop-os.localdomain>
2022-12-20 15:40:06 +0100

    Part 1

---
+++ src/day10/mod.rs
@@ -0,0 +1,36 @@
+pub fn part1(input: String) -> i32 {
+    let mut cycle = 0;
+    let mut register = 1;
+    let mut register_values: Vec<i32> = vec![];
+    let cycles_strength = [20, 60, 100, 140, 180, 220];
+    input.split('\n').for_each(|line| {
+        let words = line.split(' ').collect::<Vec<&str>>();
+        match words[0] {
+            "noop" => {
+                println!("noop - cycle: {}, register: {}", cycle, register);
+                cycles_strength.iter().for_each(|x| {
+                    if (cycle..(cycle + 1)).contains(x) {
+                        register_values.push(register)
+                    }
+                });
+                cycle += 1;
+            }
+            "addx" => {
+                println!("addx - cycle: {}, register: {}", cycle, register);
+                cycles_strength.iter().for_each(|x| {
+                    if (cycle..(cycle + 2)).contains(x) {
+                        register_values.push(register)
+                    }
+                });
+                register += words[1].parse::<i32>().unwrap();
+                cycle += 2;
+            }
+            _ => panic!("Unknown operation"),
+        };
+    });
+    println!("register values: {:?}", register_values);
+    return register_values
+        .iter()
+        .zip(cycles_strength.iter())
+        .fold(0, |acc, (left, right)| acc + left * right);
+}
