a35e778fecd377aec92114ee585d9a8497691f26
Maciej Kozuszek <maciejkozuszek@pop-os.localdomain>
2022-12-24 13:37:13 +0100

    Part 2

--- src/day9/mod.rs
+++ src/day9/mod.rs
@@ -1,7 +1,11 @@
-use std::collections::HashSet;
+use std::{cell::RefCell, collections::HashSet};
 
 fn distance(left: (i32, i32), right: (i32, i32)) -> (u32, u32) {
     return (left.0.abs_diff(right.0), left.1.abs_diff(right.1));
+}
+
+fn delta(left: (i32, i32), right: (i32, i32)) -> (i32, i32) {
+    return (left.0 - (right.0), left.1 - (right.1));
 }
 
 pub fn part1(input: String) -> usize {
@@ -14,53 +18,21 @@ pub fn part1(input: String) -> usize {
     input.split('\n').for_each(|line| {
         let words = line.split(' ').collect::<Vec<&str>>();
         let steps = words[1].parse::<i32>().unwrap();
-        match words[0] {
-            "U" => {
-                for step in 0..steps {
-                    let next_head = (head.0, head.1 + 1);
-                    tail = match distance(next_head, tail) {
-                        (0..=1, 0..=1) => tail,
-                        _ => (next_head.0, next_head.1 - 1),
-                    };
-                    head = next_head;
-                    visited_points.insert(tail);
-                }
-            }
-            "R" => {
-                for step in 0..steps {
-                    let next_head = (head.0 + 1, head.1);
-                    tail = match distance(next_head, tail) {
-                        (0..=1, 0..=1) => tail,
-                        _ => (next_head.0 - 1, next_head.1),
-                    };
-                    head = next_head;
-                    visited_points.insert(tail);
-                }
-            }
-            "D" => {
-                for step in 0..steps {
-                    let next_head = (head.0, head.1 - 1);
-                    tail = match distance(next_head, tail) {
-                        (0..=1, 0..=1) => tail,
-                        _ => (next_head.0, next_head.1 + 1),
-                    };
-                    head = next_head;
-                    visited_points.insert(tail);
-                }
-            }
-            "L" => {
-                for step in 0..steps {
-                    let next_head = (head.0 - 1, head.1);
-                    tail = match distance(next_head, tail) {
-                        (0..=1, 0..=1) => tail,
-                        _ => (next_head.0 + 1, next_head.1),
-                    };
-                    head = next_head;
-                    visited_points.insert(tail);
-                }
-            }
-            _ => panic!("err"),
-        };
+        for _step in 0..steps {
+            let next_head = match words[0] {
+                "U" => (head.0, head.1 + 1),
+                "R" => (head.0 + 1, head.1),
+                "D" => (head.0, head.1 - 1),
+                "L" => (head.0 - 1, head.1),
+                _ => panic!("Invalid direction symbol!"),
+            };
+            tail = match distance(next_head, tail) {
+                (0..=1, 0..=1) => tail,
+                _ => head,
+            };
+            head = next_head;
+            visited_points.insert(tail);
+        }
         visited_points.insert(tail);
     });
 
@@ -76,80 +48,35 @@ pub fn part2(input: String) -> usize {
     input.split('\n').for_each(|line| {
         let words = line.split(' ').collect::<Vec<&str>>();
         let steps = words[1].parse::<i32>().unwrap();
-        match words[0] {
-            "U" => {
-                for step in 0..steps {
-                    for points in points.chunks_mut(2) {
-                        let [prev, next] = points;
-                        let next_next = (next.0, next.1 + 1);
-                        let next_prev = match distance(next.clone(), prev.clone()) {
-                            (0..=1, 0..=1) => prev,
-                            _ => &mut (next_next.0.clone(), next_next.1.clone() - 1),
-                        };
-                        visited_points.insert(prev.clone());
+        for step in 0..steps {
+            let mut head = points.first_mut().unwrap();
+            let mut next_head = match words[0] {
+                "U" => (head.0, head.1 + 1),
+                "R" => (head.0 + 1, head.1),
+                "D" => (head.0, head.1 - 1),
+                "L" => (head.0 - 1, head.1),
+                _ => *head,
+            };
+            *head = next_head;
+            for i in 1..10 {
+                if let Ok([tail, next]) = points.get_many_mut([i, i - 1]) {
+                    let row_diff: i32 = next.0 - tail.0;
+                    let col_diff: i32 = next.1 - tail.1;
+
+                    if row_diff == 0 && col_diff.abs() > 1 {
+                        *tail = (tail.0, tail.1 + col_diff.signum());
+                    } else if col_diff == 0 && row_diff.abs() > 1 {
+                        *tail = (tail.0 + row_diff.signum(), tail.1);
+                    } else if row_diff.abs() > 1 || col_diff.abs() > 1 {
+                        *tail = (tail.0 + row_diff.signum(), tail.1 + col_diff.signum());
+                    }
+
+                    if i == 9 {
+                        visited_points.insert(tail.clone());
                     }
                 }
             }
-            "R" => {
-                for step in 0..steps {
-                    for [prev, next] in points.chunks_exact_mut(2) {
-                        next = &mut (next.0 + 1, next.1);
-                        prev = match distance(*next, *prev) {
-                            (0..=1, 0..=1) => prev,
-                            _ => &mut (next.0 - 1, next.1),
-                        };
-                        visited_points.insert(prev.clone());
-                    }
-                    // let next_head = (head.0 + 1, head.1);
-                    // tail = match distance(next_head, tail) {
-                    //     (0..=1, 0..=1) => tail,
-                    //     _ => (next_head.0 - 1, next_head.1),
-                    // };
-                    // head = next_head;
-                    // visited_points.insert(tail);
-                }
-            }
-            "D" => {
-                for step in 0..steps {
-                    for [prev, next] in points.chunks_exact_mut(2) {
-                        next = &mut (next.0, next.1 - 1);
-                        prev = match distance(*next, *prev) {
-                            (0..=1, 0..=1) => prev,
-                            _ => &mut (next.0, next.1 + 1),
-                        };
-                        visited_points.insert(prev.clone());
-                    }
-                    // let next_head = (head.0, head.1 - 1);
-                    // tail = match distance(next_head, tail) {
-                    //     (0..=1, 0..=1) => tail,
-                    //     _ => (next_head.0, next_head.1 + 1),
-                    // };
-                    // head = next_head;
-                    // visited_points.insert(tail);
-                }
-            }
-            "L" => {
-                for step in 0..steps {
-                    for [prev, next] in points.chunks_exact_mut(2) {
-                        next = &mut (next.0 - 1, next.1);
-                        prev = match distance(*next, *prev) {
-                            (0..=1, 0..=1) => prev,
-                            _ => &mut (next.0 + 1, next.1),
-                        };
-                        visited_points.insert(prev.clone());
-                    }
-                    // let next_head = (head.0 - 1, head.1);
-                    // tail = match distance(next_head, tail) {
-                    //     (0..=1, 0..=1) => tail,
-                    //     _ => (next_head.0 + 1, next_head.1),
-                    // };
-                    // head = next_head;
-                    // visited_points.insert(tail);
-                }
-            }
-            _ => panic!("err"),
-        };
-        // visited_points.insert(tail);
+        }
     });
 
     return visited_points.len();
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![feature(iter_array_chunks)]
 #![feature(exclusive_range_pattern)]
+#![feature(get_many_mut)]
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
@@ -35,7 +36,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     //     println!("highest: {}", exp.highest_calories());
     //     println!("highest top 3: {}", exp.highest_3_calories());
     // }
-    let mut file = File::open("./src/day10/input.txt")?;
+    let mut file = File::open("./src/day9/input.txt")?;
     let mut data = String::new();
     file.read_to_string(&mut data)?;
 
@@ -45,8 +46,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     // let day2_part2_result = day2::part2(data);
     // println!("total score: {}", day2_part2_result);
 
-    let day10_part2_result = day10::part2(data);
-    // println!("total priority: {}", day10_part2_result);
+    let day9_part2_result = day9::part2(data);
+    println!("total priority: {}", day9_part2_result);
     Ok(())
 }
 
